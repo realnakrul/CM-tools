ee4126fe0b4c086b0bd7630d099b491d2d9f7e2f
realnakrul <21193002+realnakrul@users.noreply.github.com>
2020-06-17 11:36:29 +0400

    Initial commit

---
+++ group.py
@@ -0,0 +1,29 @@
+from sys import argv
+import tools
+import pandas
+import xlrd
+from pprint import pprint
+
+
+DEVICES = ['CR SW 01', 'CR SW 02']
+
+
+def main():
+    if tools.check_input(argv[1]):
+        matrix_input = argv[1]
+        matrix_output = f'grp_{matrix_input}'
+        print(f'Output file: {matrix_output}')
+        # matrix = pandas.read_excel(matrix_input)
+        # print(matrix)
+        wb = xlrd.open_workbook(matrix_input)
+        sheet = tools.select_sheet(wb)
+        print(sheet.name)
+        raw_matrix_list = tools.read_sheet(sheet)
+        clean_matrix_list = tools.clean_list(raw_matrix_list)
+        # pprint(clean_matrix_list)
+        group_list = tools.group_by_device(DEVICES, clean_matrix_list)
+        tools.write_to_excel(matrix_output, sheet.name, group_list)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tools.py
@@ -0,0 +1,77 @@
+from os import path
+import xlrd
+import xlwt
+
+
+def check_input(file):
+    result = path.exists(file)
+    print(f'Input file: {file}')
+    if result:
+        print('Found.')
+    else:
+        print('Not found.')
+    return result
+
+
+def select_sheet(book):
+    print('Available sheets: ')
+    for ind, sheet in enumerate(book.sheet_names(), 1):
+        print(f'{ind}. {sheet}')
+    correct = False
+    while not correct:
+        try:
+            sheet_ind = int(input(f'Select sheet (1 - {len(book.sheet_names())}): '))
+            sheet = book.sheet_by_index(sheet_ind - 1)
+            correct = True
+        except ValueError:
+            print('Select sheet number')
+        except IndexError:
+            print(f'Select sheet number in range (1 - {len(book.sheet_names())})')
+        except Exception as e:
+            print(type(e), e)
+    return sheet
+
+
+def read_sheet(sheet):
+    matrix_list = []
+    row_list = []
+    for row in range(0, sheet.nrows):
+        row_list = []
+        # print(sheet.row(row))
+        for cell in sheet.row(row):
+            # print(cell.value)
+            row_list.append(cell.value)
+        matrix_list.append(row_list)
+    # for row in wb.sheet_by_index(0).rows
+    return matrix_list
+
+
+def clean_list(raw_list):
+    clean = []
+    for line in raw_list:
+        if line[1]:
+            clean.append(line)
+    return clean
+
+
+def group_by_device(devices, matrix):
+    result = []
+    for device in devices:
+        result.append([device])
+        for line in matrix:
+            if device in line[0]:
+                result.append(line)
+    return result
+
+
+def write_to_excel(file, sheet_name, data):
+    workbook = xlwt.Workbook()
+    sheet = workbook.add_sheet(sheet_name)
+    for line_ind, line in enumerate(data):
+        for cel_ind, cell in enumerate(line):
+            sheet.write(line_ind, cel_ind, cell)
+    workbook.save('grp.xls')
+
+
+
+
