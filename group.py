26cb1acc93f4225d856b44ea9046e4a6794f19ab
realnakrul <21193002+realnakrul@users.noreply.github.com>
2020-06-23 17:12:20 +0400

    Initial openpyxl migraton

--- group.py
+++ group.py
@@ -1,8 +1,10 @@
 from sys import argv
 import tools
 import pandas
-import xlrd
+# import xlrd
 from pprint import pprint
+from openpyxl import Workbook
+from openpyxl import load_workbook
 
 
 def main():
@@ -12,15 +14,19 @@ def main():
         print(f'Output file: {matrix_output}')
         # matrix = pandas.read_excel(matrix_input)
         # print(matrix)
-        wb = xlrd.open_workbook(matrix_input)
+        # wb = xlrd.open_workbook(matrix_input)
+        wb = load_workbook(matrix_input)
         sheet = tools.select_sheet(wb)
-        # print(sheet.name)
+        # print(sheet.title)
+        print(sheet.max_column)
+        print(sheet.max_row)
         raw_matrix_list = tools.read_sheet(sheet)
         clean_matrix_list = tools.clean_list(raw_matrix_list)
         devices = tools.get_devices(clean_matrix_list)
         # pprint(clean_matrix_list)
+        tools.split_interfaces(clean_matrix_list)
         group_list = tools.group_by_device(devices, clean_matrix_list)
-        tools.write_to_excel(matrix_output, sheet.name, group_list)
+        tools.write_to_excel(matrix_output, sheet.title, group_list)
 
 
 if __name__ == '__main__':
--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 from os import path
 import xlrd
-import xlwt
+# import xlwt
+from openpyxl import Workbook
 
 
 def check_input(file):
@@ -15,18 +16,19 @@ def check_input(file):
 
 def select_sheet(book):
     print('Available sheets: ')
-    for ind, sheet in enumerate(book.sheet_names(), 1):
+    for ind, sheet in enumerate(book.sheetnames, 1):
         print(f'{ind}. {sheet}')
     correct = False
     while not correct:
         try:
-            sheet_ind = int(input(f'Select sheet (1 - {len(book.sheet_names())}): '))
-            sheet = book.sheet_by_index(sheet_ind - 1)
+            sheet_ind = int(input(f'Select sheet (1 - {len(book.sheetnames)}): '))
+            # sheet = book.sheet_by_index(sheet_ind - 1)
+            sheet = book.worksheets[sheet_ind - 1]
             correct = True
         except ValueError:
             print('Select sheet number')
         except IndexError:
-            print(f'Select sheet number in range (1 - {len(book.sheet_names())})')
+            print(f'Select sheet number in range (1 - {len(book.sheetnames)})')
         except Exception as e:
             print(type(e), e)
     return sheet
@@ -35,10 +37,12 @@ def select_sheet(book):
 def read_sheet(sheet):
     matrix_list = []
     row_list = []
-    for row in range(0, sheet.nrows):
+    #for row in range(0, sheet.nrows):
+    for row in sheet.iter_rows():
         row_list = []
-        # print(sheet.row(row))
-        for cell in sheet.row(row):
+        # print(row)
+        #for cell in sheet.row(row):
+        for cell in row:
             # print(cell.value)
             row_list.append(cell.value)
         matrix_list.append(row_list)
@@ -46,8 +50,10 @@ def read_sheet(sheet):
     return matrix_list
 
 
-def clean_list(raw_list):
+def clean_list(raw_list, header=True):
     clean = []
+    if header:
+        _ = raw_list.pop(0)
     for line in raw_list:
         if line[1]:
             clean.append(line)
@@ -67,12 +73,16 @@ def group_by_device(devices, matrix, add_name=True):
 
 
 def write_to_excel(file, sheet_name, data):
-    workbook = xlwt.Workbook()
-    sheet = workbook.add_sheet(sheet_name)
-    for line_ind, line in enumerate(data):
-        for cel_ind, cell in enumerate(line):
-            sheet.write(line_ind, cel_ind, cell)
-    workbook.save('grp.xls')
+    book = Workbook(write_only=True)
+    book.create_sheet(sheet_name)
+    sheet = book[sheet_name]
+    '''for line_ind, line in enumerate(data, 1):
+        for cel_ind, cell in enumerate(line, 1):
+            print(line_ind, cel_ind, cell)
+            sheet.cell(line_ind, cel_ind, cell)'''
+    for line in data:
+        sheet.append(line)
+    book.save(file)
 
 
 def get_devices(matrix):
@@ -84,6 +94,24 @@ def get_devices(matrix):
     for d in result:
         print(d)
     return list(result)
+
+
+def split_interfaces(matrix):
+    result = []
+    for line in matrix:
+        print(line)
+        new_line = []
+        for ind, cell in enumerate(line):
+            print(ind, cell)
+            if ind == 0 or ind == 1:
+                *device, interface = cell.split()
+                new_line.extend([' '.join(device), interface])
+                print('AAA', new_line)
+            else:
+                new_line.append(cell)
+        print('new ', new_line)
+        result.append(new_line)
+    print(result)
 
 
 
